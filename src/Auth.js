b713b83952ddef52d32bd3f1ebea08eb90f405e7
gazalmighty <98213852+techsellence-dev@users.noreply.github.com>
2022-06-09 22:12:42 +0530

    clean up done

--- src/Auth.js
+++ src/Auth.js
@@ -1,51 +1,21 @@
-import React, { useState, useRef } from "react";
-import './App.css';
-import CheckCurrentUser from './CheckCurrentUser';
+import React, { useRef } from "react";
+import "./App.css";
+import CheckCurrentUser from "./CheckCurrentUser";
 
-
-
-let counter = 0;
 let currentState;
 
-
-
 function Auth() {
-  const [loginState, setLoginState] = useState(false);
-  const CurrentAuthenticatedUser=useRef(undefined);
-  const loading=useRef(false)
+  const CurrentAuthenticatedUser = useRef(undefined);
+  const loading = useRef(false);
 
-   async function checkLoginState() {
-    if(!loading.current){
-      console.log(loading.current);
-      loading.current = true;
-      console.log(loading.current);
-    counter += 1;
-    console.log('before the await function : '+ counter);
-      if(!CurrentAuthenticatedUser.current){
-          console.log("counter value inside the if is :" + counter);
-          console.log("current authenticated user is :" + CurrentAuthenticatedUser.current);
-          CurrentAuthenticatedUser.current = await CheckCurrentUser();
-          console.log(loading.current);
-          setLoginState(CurrentAuthenticatedUser.current);
-          loading.current = false;
-      }
+  async function checkLoginState() {
+    if (!CurrentAuthenticatedUser.current) {
+      CurrentAuthenticatedUser.current = await CheckCurrentUser();
     }
-    console.log('after the await function : '+ counter);
-  };
-
-  function loadingCheckLoginState(){
-    
-    checkLoginState()
-    loading.current=true
   }
+  currentState = checkLoginState();
 
-  console.log("executing before");
-  currentState = loading.current ? console.log("loading is on"):loadingCheckLoginState();
-  console.log("executing after");
-
-  return (
-<h1>Open Points</h1>
-  );
+  return <h1>Open Points</h1>;
 }
 
 export default Auth;
